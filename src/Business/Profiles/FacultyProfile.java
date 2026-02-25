b54e442e95c1432691112f555fe0564cac2f11f7
Polina Starobinets <p.starobinets@northeastern.edu>
2026-02-25 09:48:19 -0500

    Created FacultyProfile that extends Profile in the Business package to match the existing pattern Created the bridge to the university package FacultyProfile Persona Changes getRole method to return "Faculty"

---
+++ src/Business/Profiles/FacultyProfile.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Profiles;
+
+import Business.Person.Person;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class FacultyProfile extends Profile {
+
+    Person person;
+    //   Transcript transcript;
+    //   EmploymentHistroy employmenthistory;
+    
+    //bridge to Person in University Package
+    private university.Persona.Faculty.FacultyProfile universityProfile;
+
+    public FacultyProfile(Person p) {
+        super(p);
+
+
+//        transcript = new Transcript(this);
+//        employmenthistory = new EmploymentHistroy();
+    }
+
+    @Override
+    public String getRole() {
+        return "Faculty";
+    }
+
+    public boolean isMatch(String id) {
+        return person.getPersonId().equals(id);
+    }
+    
+    //methods to link Business package profile and University package Profile
+    public void linkUniversityProfile(
+            university.Persona.Faculty.FacultyProfile up){
+        this.universityProfile = up;
+    }
+    
+    public university.Persona.Faculty.FacultyProfile getUniversityProfile(){
+        return universityProfile;
+    }
+    
+
+}
